db318104410b96e9927865c45a4f6449b95ebb5c
Mathieu Grenier <mgrenier25@gmail.com>
2014-01-15 21:56:52 -0500

    added description, debugged some

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 /*	Hello World
-	By Mathieu Grenier*/
-#include<stdio.h> //include c function library stdio.h
+	Description : Prints Wazzaaa to the screen
+	By Mathieu Grenier
+*/
+#include<stdio.h> //include header stdio.h
 int main() //declare main function
 { //open { to start function
-printf("Yao the world \n"); // prints Yao the world to the screen
+printf("Wazzaaa \n"); // prints Yao the world to the screen
 return 0; // fonction should always return 0 at the end
 } // close } to end function
