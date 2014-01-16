2814a77d4a5b67e16c2ff9c0b11fe90603567d69
Mathieu Grenier <mgrenier25@gmail.com>
2014-01-15 23:00:58 -0500

    updated

--- strarr.c
+++ strarr.c
@@ -7,7 +7,9 @@
 
 int main() // declare main function
 {
+	int i;
 	char ca[10], *sp; //declare the ca character array, declare sp pointer as character
+	i = 0;
 	sp = "Hello"; // sets the value of sp to Hello
 	ca[0] = 'H'; // sets the value of ca[0] to H
 	ca[1] = 'e'; // sets the value of ca[1] to e
@@ -15,7 +17,13 @@ int main() // declare main function
 	ca[3] = 'l'; // sets the value of ca[3] to l
 	ca[4] = 'o'; // sets the value of ca[4] to o
 	ca[5] = 0; // sets the value of ca[5] and following members of the array to 0(null)
-	printf("%s\n", ca); // prints the value of the whole ca character array to the screen on the same line and escape to a new line
-	printf("%s\n", sp); // prints the value of the character pointer sp to the screen and escape to a new line
+	printf("\n%s\n\n", ca); // prints the value of the whole ca character array to the screen on the same line and escape to a new line
+	do // starts loop to print value from integer array a on different lines
+	{
+	printf("%c\n", ca[i]); // prints the value of the integer i from array a and escape to a new line
+	i++;
+	}
+	while (i <= ca[i]); // run loop as long as integer i is less than or equals to 4
+	printf("\n%s\n", sp); // prints the value of the character pointer sp to the screen and escape to a new line
 return 0; // function should always return 0 at the end
 }
