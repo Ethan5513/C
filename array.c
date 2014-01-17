2ba1afe1f9b1c1434cbfb9bfd997f00c46703c44
Mathieu Grenier <mgrenier25@gmail.com>
2014-01-16 22:51:08 -0500

    playing with arrays

--- array.c
+++ array.c
@@ -7,19 +7,31 @@
 
 int main() // declare main function
 {
+	int r1=rand() % 4,r2=rand() % 4,r3=rand() % 4;
 	int i; // declare i as integer number
+	int b;
 	int a[5]; // declare an array of 5 integers in a
 	a[0] = 12; // sets the value of a[0] to 12
 	a[1] = 23; // sets the value of a[1] to 23
 	a[2] = 34; // sets the value of a[2] to 34
 	a[3] = 45; // sets the value of a[3] to 45
 	a[4] = 56; // sets the value of a[4] to 56
-	printf("%d %d %d %d %d\n", a[0], a[1], a[2], a[3], a[4]); // prints the value of the whole a array to the screen, on the same line
+	printf("%d %d %d %d %d %d %d %d\n", a[0], a[1], a[2], a[3], a[4], a[r1], a[r2], a[r3]); // prints the value of the whole a array to the screen, on the same line
+printf("\n");
+i=0;
+b=4;
 	do // starts loop to print value from integer array a on different lines
-	{
-	printf("%d\n", a[i]); // prints the value of the integer i from array a and escape to a new line
-	i++; // increment i by 1
-	}
-	while (i <= 4); // run loop as long as integer i is less than or equals to 4
-return 0; // function should always return 0 at the end
+	    {
+	    printf("%d %d %d %d %d\n", a[i], a[b], a[r1], a[r2], a[r3]); // prints the value of the integer i from array a and escape to a new line
+	    }
+	while (r1=rand() % 4,r3=rand() % 4,b--, i++, i <= 4, b >= 0); // run loop as long as integer i is less than or equals to 4
+b=0;
+i=4;
+      printf("\n");
+	do // starts loop to print value from integer array a on different lines
+	    {
+		printf("%d %d %d %d %d\n", a[i], a[b], a[r1], a[r2], a[r3]);
+		}
+		while (r2=rand() % 4,r3=rand() % 4,b++,i--, i >=0, b<=4);
+		return 0; // function should always return 0 at the end
 }
--- strarr.c
+++ strarr.c
@@ -7,9 +7,11 @@
 
 int main() // declare main function
 {
-	int i;
+	
+	int i,o,u,a,b; // declares i o
 	char ca[10], *sp; //declare the ca character array, declare sp pointer as character
-	i = 0;
+	i = 0; // sets the value of i to 0
+	o = 4; // sets the value of o to 4
 	sp = "Hello"; // sets the value of sp to Hello
 	ca[0] = 'H'; // sets the value of ca[0] to H
 	ca[1] = 'e'; // sets the value of ca[1] to e
@@ -18,12 +20,35 @@ int main() // declare main function
 	ca[4] = 'o'; // sets the value of ca[4] to o
 	ca[5] = 0; // sets the value of ca[5] and following members of the array to 0(null)
 	printf("\n%s\n\n", ca); // prints the value of the whole ca character array to the screen on the same line and escape to a new line
-	do // starts loop to print value from integer array a on different lines
+    printf("%c %c %c %c %c\n", ca[0], ca[1],ca[2],ca[3],ca[4]); // prints the value of the character array ca to the screen on the same line
+	printf("%c %c %c %c %c\n", ca[4],ca[3],ca[2],ca[1],ca[0]); // prints olleH to the screen to the screen on the same line
+	printf("%c %c %c %c %c\n",sp[0], sp[1], sp[2], sp[3], sp[4]);
+ 	do // starts loop to print value from integer array a on different lines
 	{
-	printf("%c\n", ca[i]); // prints the value of the integer i from array a and escape to a new line
-	i++;
+	printf("%c %c \n", ca[i], ca[o]); // prints the value of the integer i from array a and escape to a new line
+	i++; // increments i by 1
+	o--;
 	}
-	while (i <= ca[i]); // run loop as long as integer i is less than or equals to 4
+	while (i <= 4, o >= 0); // run loop as long as integer i is less than or equals to 4
+	do
+	{
+	printf("%c %c \n", ca[o], ca[i]);
+	i--;	
+	o++;
+	}
+	while (i >= 0, o <= 4);
+	i =0;
+	o =4;
+	do
+	{
+	printf("%c %c %c %c %c\n", ca[i], ca[o], ca[u], ca[a], ca[b]);
+	i++;
+	o--;
+	u=rand() % 4;
+	a=rand() % 4;
+	b=rand() % 4; 
+	}
+	while ( i <= 4, o >= 0);
 	printf("\n%s\n", sp); // prints the value of the character pointer sp to the screen and escape to a new line
 return 0; // function should always return 0 at the end
 }
